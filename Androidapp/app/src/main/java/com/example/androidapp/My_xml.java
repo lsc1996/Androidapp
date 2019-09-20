55e902b8488399374b5c0a79d7116beb6ffe331d
3rd_sw1 <3rd_sw1@ant-online.cn>
2019-09-20 12:03:45 +0800

    第一次提交

---
+++ Androidapp/app/src/main/java/com/example/androidapp/My_xml.java
@@ -0,0 +1,13 @@
+package com.example.androidapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class My_xml extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_xml);
+    }
+}
